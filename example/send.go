d32eb50fa092f391a54c0ad8122a4aad73ae98d0
fhasheminasab <hasheminasab97@gmail.com>
2022-02-16 13:07:12 +0330

    added status check

--- example/send.go
+++ example/send.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	c := ghasedak.NewClient("05624f7710742db14636cc4700ec5a4464a4a467efb998f80e4820282ec4d5bc", "10008566")
 
-	// sms := c.Send("!", "09120581875")
-	// fmt.Println(sms.Code)
-	// fmt.Println(sms.Message)
+	sms := c.Send("س", "09120581875")
+	fmt.Println(sms.Code)
+	fmt.Println(sms.Message)
 	statuscheck := c.Status("2914845496", "1")
 	fmt.Println(statuscheck.Code)
 	fmt.Println(statuscheck.Message)
